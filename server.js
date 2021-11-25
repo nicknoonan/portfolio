e4aa4b03b55f794af64383c1075ba00129f804ea
nicholas noonan <nicknoonan86@gmail.com>
2021-11-25 08:59:10 -0800

    added server script

---
+++ server.js
@@ -0,0 +1,23 @@
+//modules from other files
+const path = require('path');
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+//
+//middleware
+const app = express();
+app.use(express.json());
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, './build')));
+
+//use router module
+// app.use('/', router);
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, './build', 'index.html'));
+});
+
+//listen at PORT
+console.log('Listening on port ' + PORT + ' for requests');
+app.listen(PORT);
