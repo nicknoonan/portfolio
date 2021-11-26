18c0a4264ae131ba9a6d0331cbca6e21863a8a84
nick noonan <nicknoonan86@gmail.com>
2021-11-26 19:33:11 +0000

    test webhook

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 cd /home/nickdev/portfolio
 sudo docker-compose build
-sudo docker stop nick_portfolio_container > /dev/null
-sudo docker container rm nick_portfolio_container > /dev/null
+sudo docker stop nick_portfolio_container
+sudo docker container rm nick_portfolio_container
 sudo docker create -p 80:80 --name nick_portfolio_container nick_portfolio:latest
-sudo docker start nick_portfolio
+sudo docker start nick_portfolio_container
