8b441071014cbe99062c8788a0e7288aa6a9e2fc
nick noonan <nicknoonan86@gmail.com>
2021-11-26 19:22:27 +0000

    update publish script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,5 @@
-npm run build
 sudo docker-compose build
-sudo docker run -p 80 
+sudo docker stop nick_portfolio_container > /dev/null
+sudo docker container rm nick_portfolio_container > /dev/null
+sudo docker create -p 80:80 --name nick_portfolio_container nick_portfolio:latest
+sudo docker start nick_portfolio
